7e8e9af146ec95824e2e2a8258b00dcccd05a2eb
MXCR-cpu <62783421+MXCR-cpu@users.noreply.github.com>
2023-01-18 01:39:39 +0100

    Correcting tests.lua V2

--- lua/tests.lua
+++ lua/tests.lua
@@ -7,8 +7,9 @@ M.run_tests = function()
 	local string_sentences = { "here is an example", "hereIsAnExample", "here_is_an_example", "here-is-an-example" }
 	local cases = { "camel_case", "snake_case", "kebab_case" }
 	for index = 1, #cases do
-		if utils[cases[index]]("To", {string_sentences[1]}, "") ~= string_sentences[1 + index] then
-			print("case " .. cases[index] .. ": failed to convert to case sentence")
+		local result = utils[cases[index]]("To", {string_sentences[1]}, "")
+		if result ~= string_sentences[1 + index] then
+			print("case " .. cases[index] .. ": failed to convert to case sentence, " .. string_sentences[1] .. " x--> " .. result)
 		end
 	end
 end
