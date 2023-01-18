2a753e6ec3a0b6e02e795afda8d5306b98d56f1e
MXCR-cpu <62783421+MXCR-cpu@users.noreply.github.com>
2023-01-18 01:45:59 +0100

    Correcting tests.lua V4

--- lua/tests.lua
+++ lua/tests.lua
@@ -6,11 +6,13 @@ M = {}
 M.run_tests = function()
 	local string_sentences = { "here is an example", "hereIsAnExample", "here_is_an_example", "here-is-an-example" }
 	local cases = { "camel_case", "snake_case", "kebab_case" }
+	local passed_test = true
 	for index = 1, #cases do
 		local result = utils[cases[index]]("To", { string_sentences[1] }, "")
 		if result ~= string_sentences[1 + index] then
+			passed_test = false
 			print(
-				"case "
+				"Boop: case "
 					.. cases[index]
 					.. ': failed to convert to case sentence, "'
 					.. string_sentences[1]
@@ -19,6 +21,9 @@ M.run_tests = function()
 					.. '"'
 			)
 		end
+	end
+	if passed_test then
+		print("Boop: All Tests Passed ✓")
 	end
 end
 
